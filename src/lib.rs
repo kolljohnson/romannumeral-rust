fdd563e88df64e3aec2b4a7435ad6a462a83d8a2
kolljohnson <kjohnson@pillartechnology.com>
2017-07-17 13:17:06 -0500

    Implement returning numeral one

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,19 @@
 #[cfg(test)]
 mod tests {
+    use super::*;
+    
     #[test]
     fn it_works() {
     }
 
-    
+    #[test]
+    fn number_one_returns_numeral_one() {
+        assert_eq!("I", roman_convert(1));
+    }
 }
+
+fn roman_convert(number: i32) -> String {
+    let numeral = String::from("I");
+    numeral
+}
+
