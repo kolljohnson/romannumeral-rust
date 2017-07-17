08eed073fb6c31755ade94ea402469fc6cff36ca
kolljohnson <kjohnson@pillartechnology.com>
2017-07-17 13:29:49 -0500

    Implement roman numeral for two

--- src/lib.rs
+++ src/lib.rs
@@ -10,10 +10,19 @@ mod tests {
     fn number_one_returns_numeral_one() {
         assert_eq!("I", roman_convert(1));
     }
+
+    #[test]
+    fn number_two_returns_numeral_two() {
+        assert_eq!("II", roman_convert(2));
+    }
 }
 
 fn roman_convert(number: i32) -> String {
-    let numeral = String::from("I");
+    let mut numeral = String::new();
+
+    for x in 0..number {
+        numeral.push_str("I");
+    }
     numeral
 }
 
